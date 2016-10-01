0815d435d2d9754ec70b5f3d2afa905f92b0c1bb
wisnuko13 <wisnuko13@gmail.com>
2016-10-01 15:09:16 +0700

    Udacity Making Card

---
+++ HappyBirthdayCard/app/src/main/java/com/wisnu_krn/happybirthdaycard/MainActivity.java
@@ -0,0 +1,19 @@
+package com.wisnu_krn.happybirthdaycard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView mImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        mImageView = (ImageView) findViewById(R.id.gambar);
+//        mImageView.setImageResource(R.drawable.jokerj);
+    }
+}
