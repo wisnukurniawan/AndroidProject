14270fe0f2564a620eb1b5e18084b15ed1ce1b42
wisnuko13 <wisnuko13@gmail.com>
2016-10-06 19:24:38 +0700

    versi 1.0.0

---
+++ BasketSkor/app/src/main/java/com/wisnu_krn/basketskor/MainActivity.java
@@ -0,0 +1,113 @@
+package com.wisnu_krn.basketskor;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private int scoreTeamA = 0;
+    private int scoreTeamB = 0;
+
+    final int POINTS_FOR_FREE_THROW = 1;
+    final int POINTS_FOR_TWO_POINTS = 2;
+    final int POINT_FOR_THREE_POINTS = 3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button forAOnePoint = (Button) findViewById(R.id.a_poin_1);
+        Button forATwoPoint = (Button) findViewById(R.id.a_poin_2);
+        Button forAThreePoint = (Button) findViewById(R.id.a_poin_3);
+        Button forBOnePoint = (Button) findViewById(R.id.b_poin_1);
+        Button forBTwoPoint = (Button) findViewById(R.id.b_poin_2);
+        Button forBThreePoint = (Button) findViewById(R.id.b_poin_3);
+        Button reset = (Button) findViewById(R.id.reset);
+
+        forATwoPoint.setOnClickListener(this);
+        forAOnePoint.setOnClickListener(this);
+        forAThreePoint.setOnClickListener(this);
+        forBTwoPoint.setOnClickListener(this);
+        forBOnePoint.setOnClickListener(this);
+        forBThreePoint.setOnClickListener(this);
+        reset.setOnClickListener(this);
+
+    }
+
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.tim_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.tim_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.a_poin_1 :
+                addOneForTeamA();
+                break;
+            case R.id.a_poin_2 :
+                addTwoForTeamA();
+                break;
+            case R.id.a_poin_3 :
+                addThreeForTeamA();
+                break;
+            case R.id.b_poin_1 :
+                addOneForTeamB();
+                break;
+            case R.id.b_poin_2 :
+                addTwoForTeamB();
+                break;
+            case R.id.b_poin_3 :
+                addThreeForTeamB();
+                break;
+            case R.id.reset :
+                resetSkor();
+        }
+    }
+
+    public void addOneForTeamA() {
+        scoreTeamA = scoreTeamA + POINTS_FOR_FREE_THROW;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void addTwoForTeamA() {
+        scoreTeamA = scoreTeamA + POINTS_FOR_TWO_POINTS;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void addThreeForTeamA() {
+        scoreTeamA = scoreTeamA + POINT_FOR_THREE_POINTS;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void addOneForTeamB() {
+        scoreTeamB = scoreTeamB + POINTS_FOR_FREE_THROW;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addTwoForTeamB() {
+        scoreTeamB = scoreTeamB + POINTS_FOR_TWO_POINTS;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addThreeForTeamB() {
+        scoreTeamB = scoreTeamB + POINT_FOR_THREE_POINTS;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void resetSkor() {
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
+    }
+
+}
