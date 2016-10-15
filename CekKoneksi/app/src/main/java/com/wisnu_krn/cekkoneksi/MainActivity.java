caa526c7992dd1d321825a25c46ef809a48b7ade
wisnuko13 <wisnuko13@gmail.com>
2016-10-15 21:07:45 +0700

    versi 1.0.0

---
+++ CekKoneksi/app/src/main/java/com/wisnu_krn/cekkoneksi/MainActivity.java
@@ -0,0 +1,42 @@
+package com.wisnu_krn.cekkoneksi;
+
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.text_1)
+    TextView mTextView;
+
+    ConnectivityManager connectivityManager;
+    NetworkInfo networkInfo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+    }
+
+    @OnClick(R.id.button_1)
+    public void cekKoneksi(){
+        String hasil;
+        connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        networkInfo = connectivityManager.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnectedOrConnecting()){
+            hasil = "tersambung";
+        } else {
+            hasil = "terputus";
+        }
+        mTextView.setText(hasil);
+    }
+}
