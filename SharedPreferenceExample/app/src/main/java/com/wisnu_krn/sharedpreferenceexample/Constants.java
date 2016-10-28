1f9c091f8b599f99f14bc60bdcb74d587bf20d5a
wisnuko13 <wisnuko13@gmail.com>
2016-10-28 08:30:05 +0700

    Sebelum di handle

---
+++ SharedPreferenceExample/app/src/main/java/com/wisnu_krn/sharedpreferenceexample/Constants.java
@@ -0,0 +1,10 @@
+package com.wisnu_krn.sharedpreferenceexample;
+
+/**
+ * Created by private on 28/10/2016.
+ */
+
+public class Constants {
+    public static final String PREF_FILE = "com.wisnu_krn.que_a.preferences";
+    public static final String DEFAULT_STRING = "";
+}
---
+++ SharedPreferenceExample/app/src/main/java/com/wisnu_krn/sharedpreferenceexample/MainActivity.java
@@ -0,0 +1,41 @@
+package com.wisnu_krn.sharedpreferenceexample;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+
+import static com.wisnu_krn.sharedpreferenceexample.Constants.DEFAULT_STRING;
+import static com.wisnu_krn.sharedpreferenceexample.Constants.PREF_FILE;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText komentarField;
+
+    private SharedPreferences mSharedPreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        komentarField = (EditText) findViewById(R.id.komentar);
+    }
+
+    public void saveString(String value) {
+        mSharedPreferences = getSharedPreferences(PREF_FILE, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = mSharedPreferences.edit();
+        editor.putString("Data", value);
+        editor.apply();
+    }
+    public void loadString() {
+        mSharedPreferences = getSharedPreferences(PREF_FILE, Context.MODE_PRIVATE);
+        String savedValue = mSharedPreferences.getString("Data", DEFAULT_STRING);
+        if (savedValue.equals(DEFAULT_STRING)){
+            //Toast.makeText(getApplicationContext(), "Nilai KOSONG", Toast.LENGTH_LONG).show();
+        }else {
+            //displayPengumuman(savedValue);
+            //setCursorInEnd();
+        }
+    }
+}
